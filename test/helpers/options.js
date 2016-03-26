b64a952b544db0fece8a6ef127010140495131b2
Alex Grant <alex@localnerve.com>
2016-03-26 16:31:01 -0400

    revert test phantomjs option decoration

--- test/helpers/options.js
+++ test/helpers/options.js
@@ -2,24 +2,30 @@
  * options.js
  *
  * A test helper to decorate html-snapshots options
- * 
+ *
  * phantomjs
  * If a global phantomjs is defined, decorates html-snapshots options to specify that global
  * In some test environments (travis), local phantomjs will not install if a global is found.
  */
 
+// @0.12.0, on Travis-ci phantomjs2 WILL install local WITH the older global.
+// So reverting back to decorateLocal only.
+// THIS COULD CHANGE... stay on top.
+
+/*
 var spawn = require("child_process").spawn;
 
 function decorateGlobal (options) {
     options.phantomjs = "phantomjs";
     return options;
 }
-
+*/
 function decorateLocal(options) {
   // local is the default
   return options;
 }
 
+/*
 var cp = spawn("phantomjs", ["--version"]);
 cp.on("error", function(err) {
   module.exports.decorate = decorateLocal;
@@ -27,4 +33,9 @@ cp.on("error", function(err) {
 
 module.exports = {
   decorate: decorateGlobal
+};
+*/
+
+module.exports = {
+  decorate: decorateLocal
 };
