35b3f5c2f2324481a5c32ffa064caec5bb3dc16a
Alex Grant <alex@localnerve.com>
2019-02-15 20:31:14 -0500

    html-snapshots@0.15.2, close #71, eslint, fix lint errors

--- test/mocha/html-snapshots/use-jquery.js
+++ test/mocha/html-snapshots/use-jquery.js
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2013 - 2019, Alex Grant, LocalNerve, contributors
  */
-/* global module, require, process, clearInterval, setTimeout, it */
+/* global module, require, it */
 var assert = require("assert");
 var path = require("path");
 var _ = require("lodash");
