a584cfe539ce4f758fa202fc3dcc42d3faff60f7
Alex Grant <alex@localnerve.com>
2019-06-07 17:05:45 -0400

    v0.16.0, fix mustBeError test helper for nodeJS v10 assert changes

--- test/helpers/result.js
+++ test/helpers/result.js
@@ -5,12 +5,23 @@
  */
 var assert = require("assert");
 
-function mustBeError(err) {
+/**
+ * NodeJS v8 ifError throws if value is truthy
+ * NodeJS v10 ifError throws if value is NOT undefined or null.
+ * So to cover both, added falsy check.
+ *
+ * @param {*} err 
+ */
+function notFalsy (err) {
+  return !!err;
+}
+
+function mustBeError (err) {
   assert.throws(
-    function() {
-      assert.ifError(err);
+    function () {
+      notFalsy(err) && assert.ifError(err);
     },
-    function(err) {
+    function (err) {
       return !!err;
     }
   );
