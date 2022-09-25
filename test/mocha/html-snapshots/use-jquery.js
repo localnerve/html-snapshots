92e30310566f4cf74e617c313fa77c15d4340f28
Alex Grant <alex@localnerve.com>
2022-09-25 11:38:13 -0400

    puppeteer doesn't support useJQuery

--- test/mocha/html-snapshots/use-jquery.js
+++ test/mocha/html-snapshots/use-jquery.js
@@ -25,8 +25,7 @@ const useJQOutputDir = path.resolve(outputDir, "..", "useJQuery");
 
 function useJQueryTests (options) {
   const {
-    port,
-    browsers
+    port
   } = options;
 
   function createOptions (newOptions) {
@@ -36,6 +35,7 @@ function useJQueryTests (options) {
       selector: ".nojq-dynamic",
       outputDir: useJQOutputDir,
       outputDirClean: true,
+      browser: "phantomjs",
       timeout,
       useJQuery: true
     };
@@ -46,102 +46,96 @@ function useJQueryTests (options) {
   }
 
   return function () {
-    browsers.forEach(browser => {
-      it("should succeed if useJQuery=false, jQuery NOT loaded, dynamic element",
-      function (done) {
-        const options = createOptions({
-          useJQuery: false,
-          browser
-        });
+    it("should succeed if useJQuery=false, jQuery NOT loaded, dynamic element",
+    function (done) {
+      const options = createOptions({
+        useJQuery: false
+      });
 
-        ss.run(optHelp.decorate(options), (err, completed) => {
-          if (err) {
-            console.log(`@@@ error = ${err}, completed=${completed.join(',')}`);
+      ss.run(optHelp.decorate(options), (err, completed) => {
+        if (err) {
+          console.log(`@@@ error = ${err}, completed=${completed.join(',')}`);
+        }
+      })
+        .then(completed => {
+          let assertionError;
+          try {
+            assert.equal(completed.length, 1);
+            assert.equal(completed[0], path.join(useJQOutputDir, "nojq", "index.html"));
+          } catch (e) {
+            assertionError = e;
           }
+          cleanup(done, assertionError);
         })
-          .then(completed => {
-            let assertionError;
-            try {
-              assert.equal(completed.length, 1);
-              assert.equal(completed[0], path.join(useJQOutputDir, "nojq", "index.html"));
-            } catch (e) {
-              assertionError = e;
-            }
-            cleanup(done, assertionError);
-          })
-          .catch(e => {
-            checkActualFiles(e.notCompleted)
-              .then(() => {
-                cleanup(done, e || unexpectedError);
-              });
-          });
-      });
-
-      it("should succeed if useJQuery=true, jQuery loaded", function (done) {
-        const options = createOptions({
-          source: [ `http://localhost:${port}/` ],
-          selector: "#dynamic-content",
-          outputDir,
-          browser
+        .catch(e => {
+          checkActualFiles(e.notCompleted)
+            .then(() => {
+              cleanup(done, e || unexpectedError);
+            });
         });
-
-        ss.run(optHelp.decorate(options), (err, completed) => {
-          if (err) {
-            console.log('@@@ error = '+err+", completed="+completed.join(','));
-          }
-        })
-          .then(completed => {
-            var assertionError;
-            try {
-              assert.equal(completed.length, 1);
-              assert.equal(completed[0], path.join(outputDir, "index.html"));
-            } catch (e) {
-              assertionError = e;
-            }
-            cleanup(done, assertionError);
-          })
-          .catch(e => {
-            checkActualFiles(e.notCompleted)
-              .then(() => {
-                cleanup(done, e || unexpectedError);
-              });
-          });
-      });
-
-      it("should fail if useJQuery is true and no jQuery loads in target page",
-      function (done) {
-        const options = createOptions({
-          selector: "#pocs1",
-          timeout: 4000,
-          browser
-        });
-
-        const twice = _.after(2, cleanupError.bind(null, done, 0));
-
-        ss.run(optHelp.decorate(options), twice)
-          .then(unexpectedSuccess.bind(null, done))
-          .catch(twice);
-      });
-
-      it("should fail if useJQuery is false, no jQuery loads in page, BUT the element is not visible",
-      function (done) {
-        const options = createOptions({
-          selector: ".nojq-notvisible",
-          timeout: 4000,
-          browser
-        });
-
-        const twice = _.after(2, cleanupError.bind(null, done, 0));
-
-        ss.run(optHelp.decorate(options), twice)
-          .then(unexpectedSuccess.bind(null, done))
-          .catch(twice);
-      });
-
-      // most of these tests use useJQuery false and jQuery loads in target page,
-      // so not testing that combo that should always succeed as long as the
-      // selector is not dependent on jQuery.
     });
+
+    it("should succeed if useJQuery=true, jQuery loaded", function (done) {
+      const options = createOptions({
+        source: [ `http://localhost:${port}/` ],
+        selector: "#dynamic-content",
+        outputDir
+      });
+
+      ss.run(optHelp.decorate(options), (err, completed) => {
+        if (err) {
+          console.log(`@@@ error = ${err}, completed=${completed.join(',')}`);
+        }
+      })
+        .then(completed => {
+          var assertionError;
+          try {
+            assert.equal(completed.length, 1);
+            assert.equal(completed[0], path.join(outputDir, "index.html"));
+          } catch (e) {
+            assertionError = e;
+          }
+          cleanup(done, assertionError);
+        })
+        .catch(e => {
+          checkActualFiles(e.notCompleted)
+            .then(() => {
+              cleanup(done, e || unexpectedError);
+            });
+        });
+    });
+
+    it("should fail if useJQuery is true and no jQuery loads in target page",
+    function (done) {
+      const options = createOptions({
+        selector: "#pocs1",
+        timeout: 4000
+      });
+
+      const twice = _.after(2, cleanupError.bind(null, done, 0));
+
+      ss.run(optHelp.decorate(options), twice)
+        .then(unexpectedSuccess.bind(null, done))
+        .catch(twice);
+    });
+
+    it("should fail if useJQuery is false, no jQuery loads in page, BUT the element is not visible",
+    function (done) {
+      const options = createOptions({
+        selector: ".nojq-notvisible",
+        timeout: 4000
+      });
+
+      const twice = _.after(2, cleanupError.bind(null, done, 0));
+
+      ss.run(optHelp.decorate(options), twice)
+        .then(unexpectedSuccess.bind(null, done))
+        .catch(twice);
+    });
+
+    // most of these tests use useJQuery false and jQuery loads in target page,
+    // so not testing that combo that should always succeed as long as the
+    // selector is not dependent on jQuery.
   };
 }
 
